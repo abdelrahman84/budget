7abe21f61ac071bfadc446fa46f77bd5032de96d
developerNinja <a.farrag@zinad.net>
2020-04-21 01:51:24 +0200

    consuming endpoint for login

--- src/login.js
+++ src/login.js
@@ -6,16 +6,20 @@ import axios from 'axios';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Alert from '@material-ui/lab/Alert';
 var apiBaseUrl = 'http://localhost:3000/';
 
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: '',
+            email: '',
             password: '',
+            error: false,
+            errorMessage: '',
         }
         this.goToRegistration = this.goToRegistration.bind(this);
+        this.submitLogin = this.submitLogin.bind(this);
     }
     render() {
         return (
@@ -30,8 +34,8 @@ class Login extends Component {
                         </Toolbar>
                     </AppBar>
                     <div style={{ textAlign: 'center' }}>
-                        <TextField hintText="Enter your username" floatingLabelText="username"
-                            onChange={(event, newValue) => this.setState({ username: newValue })}></TextField>
+                        <TextField hintText="Enter your email" floatingLabelText="email" type="email"
+                            onChange={(event, newValue) => this.setState({ email: newValue })}></TextField>
                         <br />
 
                         <TextField type="password" hintText="Enter your Password" floatingLabelText="password"
@@ -40,6 +44,8 @@ class Login extends Component {
 
                         <RaisedButton label="Submit" primary={true} style={loginStyle}
                             onClick={(event) => this.submitLogin(event)} />
+
+                        {this.state.error ? <Alert onClose={() => { this.closeError() }} severity="error">{this.state.errorMessage}</Alert> : null}
 
                         <br />
 
@@ -61,29 +67,35 @@ class Login extends Component {
         history.push('/register');
     }
 
+    closeError() {
+        this.setState({ error: false });
+    }
+
     submitLogin(event) {
-        var self = this;
-        var payload = {
-            "username": this.state.username,
-            "password": this.state.password,
+        const { history } = this.props;
+        if (this.state.email.length > 0 && this.state.password.length > 0) {
+            var payload = {
+                "email": this.state.email,
+                "password": this.state.password,
+            }
+            axios.post(apiBaseUrl + 'login', payload).then(response => {
+                if (response.data.status === 'success') {
+                    this.setState({ error: false });
+                    localStorage.setItem('token', response.data.token)
+                    localStorage.setItem('user', JSON.stringify(response.data.user))
+                    history.push('/')
+                }
+                else if (response.data.status === 'error') {
+                  this.setState({ error: true });
+                  this.setState({ errorMessage: Array.isArray(response.data.message) ? response.data.message[0]['msg'] : response.data.message })
+                }
+            }).catch(function (error) {
+                console.log(error);
+            })
+        } else {
+            this.setState({ error: true });
+            this.setState({ errorMessage: 'please complete all fields!' });
         }
-        axios.post(apiBaseUrl + 'login', payload).then(function (response) {
-            console.log(response);
-            if (response.data.code === 200) {
-                console.log('Login successful');
-                // var uploadScreen = [];
-                // uploadScreen.push(<UploadScreen appContext = {self.props.appContext}/>)
-                // self.props.appContext.setState({loginPage: [], uploadScreen: uploadScreen})
-            }
-            else if (response.data.code === 204) {
-                console.log('user name or password don`t match')
-            }
-            else {
-                console.log('username doesn`t exist');
-            }
-        }).catch(function (error) {
-            console.log(error);
-        })
     }
 }
 
